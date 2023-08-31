03daf5d25163d6531b37d9e3e71177ecb52f6088
bool <zhenbeigg@163.com>
2023-08-31 17:30:07 +0800

    增加：请求类

---
+++ src/aop/request/AlipayCommerceEducateFacepayCloseRequest.php
@@ -0,0 +1,126 @@
+<?php
+/*
+ * @author: 布尔
+ * @name: 人脸关闭支付申请
+ * @desc: 介绍
+ * @LastEditTime: 2022-07-06 12:00:42
+ * @FilePath: \eyc3_user\Eykj\Alipay\aop\request\AlipayCommerceEducateFacepayCloseRequest.php
+ */
+
+namespace Eykj\Alipay\aop\request;
+
+/**
+ * ALIPAY API: alipay.commerce.educate.facepay.close request
+ *
+ * @author auto create
+ * @since 1.0, 2022-04-29 15:06:47
+ */
+class AlipayCommerceEducateFacepayCloseRequest
+{
+    /** 
+     * 人脸关闭支付申请
+     **/
+    private $bizContent;
+
+    private $apiParas = array();
+    private $terminalType;
+    private $terminalInfo;
+    private $prodCode;
+    private $apiVersion = "1.0";
+    private $notifyUrl;
+    private $returnUrl;
+    private $needEncrypt = false;
+
+
+    public function setBizContent($bizContent)
+    {
+        $this->bizContent = $bizContent;
+        $this->apiParas["biz_content"] = $bizContent;
+    }
+
+    public function getBizContent()
+    {
+        return $this->bizContent;
+    }
+
+    public function getApiMethodName()
+    {
+        return "alipay.commerce.educate.facepay.close";
+    }
+
+    public function setNotifyUrl($notifyUrl)
+    {
+        $this->notifyUrl = $notifyUrl;
+    }
+
+    public function getNotifyUrl()
+    {
+        return $this->notifyUrl;
+    }
+
+    public function setReturnUrl($returnUrl)
+    {
+        $this->returnUrl = $returnUrl;
+    }
+
+    public function getReturnUrl()
+    {
+        return $this->returnUrl;
+    }
+
+    public function getApiParas()
+    {
+        return $this->apiParas;
+    }
+
+    public function getTerminalType()
+    {
+        return $this->terminalType;
+    }
+
+    public function setTerminalType($terminalType)
+    {
+        $this->terminalType = $terminalType;
+    }
+
+    public function getTerminalInfo()
+    {
+        return $this->terminalInfo;
+    }
+
+    public function setTerminalInfo($terminalInfo)
+    {
+        $this->terminalInfo = $terminalInfo;
+    }
+
+    public function getProdCode()
+    {
+        return $this->prodCode;
+    }
+
+    public function setProdCode($prodCode)
+    {
+        $this->prodCode = $prodCode;
+    }
+
+    public function setApiVersion($apiVersion)
+    {
+        $this->apiVersion = $apiVersion;
+    }
+
+    public function getApiVersion()
+    {
+        return $this->apiVersion;
+    }
+
+    public function setNeedEncrypt($needEncrypt)
+    {
+
+        $this->needEncrypt = $needEncrypt;
+    }
+
+    public function getNeedEncrypt()
+    {
+        return $this->needEncrypt;
+    }
+}
